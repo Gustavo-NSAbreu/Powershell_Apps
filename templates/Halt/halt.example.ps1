8e1d20adb376e4612d525620557eb52b7758298e
Gustavo Abreu <92236522+Gustavo-NSAbreu@users.noreply.github.com>
2024-01-16 22:48:35 -0500

    Update halt.example.ps1

--- templates/Halt/halt.example.ps1
+++ templates/Halt/halt.example.ps1
@@ -1,7 +1,31 @@
+$haltDictionary = @{
+  'app1' = 'Application 1';
+}
+
 function halt {
-  param (
-    [string]$port
-  )
-	Get-Process -Id (Get-NetTCPConnection -LocalPort $port -State Listen).OwningProcess | Stop-Process
-	Write-Host "Application on port $port has stopped successfully."
+    [CmdletBinding(DefaultParameterSetName='p')]
+    param (
+        [Parameter(ParameterSetName='p', Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+        [int]$p,
+
+        [Parameter(ParameterSetName='n', Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$n
+    )
+
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'p' {
+                Get-Process -Id (Get-NetTCPConnection -LocalPort $p -State Listen).OwningProcess | Stop-Process
+                Write-Host "Application on port $p has stopped successfully."
+            }
+            'n' {
+                if ($haltDictionary.ContainsKey($n)) {
+                    Stop-Process -Name $haltDictionary[$n]
+                    Write-Host "$haltDictionary[$n] has stopped successfully."
+                } else {
+                    Write-Host "$n was not found in predefined applications."
+                }
+            }
+        }
+    }
 }
